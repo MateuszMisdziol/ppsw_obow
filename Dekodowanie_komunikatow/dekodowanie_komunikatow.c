0cbbf7fbfb524e2765f7807fe1922fc4951a21a3
Mateusz Misdzioł <misdziol@onet.pl>
2019-05-16 00:40:27 +0200

    dodanie prog obow

--- Dekodowanie_komunikatow/dekodowanie_komunikatow.c
+++ Dekodowanie_komunikatow/dekodowanie_komunikatow.c
@@ -12,7 +12,7 @@ typedef union TokenValue
 {
 	enum KeywordCode eKeyword;  //jezeli KEYWORD
 	unsigned int uiNumber;      //jezeli NUMBER
-	char * pcString;            //jezeli STRING
+	char *pcString;            //jezeli STRING
 } TokenValue;
 
 typedef struct Token
@@ -21,7 +21,7 @@ typedef struct Token
 	union TokenValue uValue;    //enum, unsigned int, char *
 } Token;
 
-struct Token asToken[MAX_TOKEN_NR];
+Token asToken[MAX_TOKEN_NR];
 
 typedef struct Keyword
 {
@@ -29,7 +29,7 @@ typedef struct Keyword
 	char cString[MAX_KEYWORD_STRING_LTH + 1];
 } Keyword;
 
-struct Keyword asKeywordList[MAX_KEYWORD_NR]=
+Keyword asKeywordList[MAX_KEYWORD_NR]=
 {
 	{RST, "reset"}, {LD, "load"}, {ST, "store"}
 };
@@ -44,80 +44,136 @@ unsigned char ucTokenNr = 0;
 
 //----------------------FUNKCJE----------------------------
 
-unsigned char ucFindTokensInString(char *pcString){
-
-	unsigned char ucTokenPointer;
-	char cCurrentChar;
-
-	enum eState {TOKEN, DELIMITER};
-
-	for (ucTokenPointer = 0;; ++ucTokenPointer){
-
-		cCurrentChar = pcString[ucTokenPointer];
-
-		switch(eState){
-			case TOKEN:
-				if(cCurrentChar == '\0'){
-					return ucTokenNr;
-				}
-				else if(cCurrentChar != ' '){}
-				else if(ucTokenNr == MAX_TOKEN_NR){
-					return ucTokenNr;
-				}
-				else{
-					eState = DELIMITER;
-				}
-				break;
-			case DELIMITER:
-				if(cCurrentChar == '\0'){
-					return ucTokenNr;
-				}
-				else if(cCurrentChar == ' '){}
-				else{
-					eState = TOKEN;
-					asToken[ucTokenNr].uValue.pcString = pcString + ucTokenPointer;
-					ucTokenNr++;
-				}
-				break;
-		}
-	}
+enum CompResult eCompareString(char pcStr1[], char pcStr2[]){
+  
+  unsigned char ucCharNumber;
+  
+  for(ucCharNumber = 0; pcStr1[ucCharNumber] == pcStr2[ucCharNumber]; ucCharNumber++){
+    if(pcStr1[ucCharNumber] == '\0'){
+      return EQUAL;
+    }
+  }
+  return DIFFERENT;
 }
 
-enum eStringToKeyword(char pcStr[], enum KeywordCode *peKeywordCode){
+unsigned char ucFindTokensInString(char *pcString){
+  
+  enum State {TOKEN, DELIMITER};
+  enum State eState = DELIMITER;
+  char cCurrentChar;
+  unsigned char ucWhichToken;
 
-	unsigned char ucTokenCounter;
+  for(ucWhichToken = 0; ;ucWhichToken++){
+    
+    cCurrentChar = pcString[ucWhichToken];
+    
+    switch(eState){
+      case TOKEN:
+        if(cCurrentChar == '\0'){
+          return ucTokenNr;
+        }
+        else if(ucTokenNr == MAX_TOKEN_NR){
+          return ucTokenNr;
+        }
+        else if(cCurrentChar == ' '){
+          eState = DELIMITER;
+        }
+        break;
+      case DELIMITER:
+        if(cCurrentChar == '\0'){
+          return ucTokenNr;
+        }
+        else if(cCurrentChar != ' '){
+          eState = TOKEN;
+          asToken[ucTokenNr].uValue.pcString = pcString + ucWhichToken;
+          ucTokenNr++;
+        }
+        break;
+    }
+  }
+}
 
-	for (ucTokenCounter = 0; ucTokenCounter < MAX_TOKEN_NR; ++ucTokenCounter)
-	{
-		if(eCompareString(pcStr, asKeywordList[ucTokenCounter]) == EQUAL){
-			*peKeywordCode = asKeywordList[ucTokenCounter].eCode;
+enum Result eStringToKeyword(char pcStr[], enum KeywordCode *peKeywordCode){
+  
+  unsigned char ucKeywordCounter;
+  
+  for(ucKeywordCounter = 0; ucKeywordCounter < MAX_KEYWORD_NR; ucKeywordCounter++){
+    if(eCompareString(pcStr, asKeywordList[ucKeywordCounter].cString) == EQUAL){
+      *peKeywordCode = asKeywordList[ucKeywordCounter].eCode;
+      return OK;
+    }
+  }
+  return ERROR;
+}
+
+enum Result eHexStringToUInt(char pcStr[], unsigned int *puiValue){
+
+	unsigned char ucCharCounter;
+
+	if((pcStr[0] != '0') | (pcStr[1] != 'x') | (pcStr[2] == '\0')){
+		return ERROR;
+	}
+
+	for(ucCharCounter = 2; ucCharCounter != '\0'; ucCharCounter++){
+		if(pcStr[ucCharCounter] == '\0'){
 			return OK;
 		}
-		else{
+		else if (ucCharCounter >= 6){
 			return ERROR;
 		}
+
+		*puiValue = *puiValue << 4;
+
+		if(pcStr[ucCharCounter] >= 'A'){
+			*puiValue = *puiValue | ((pcStr[ucCharCounter] - 'A') + 10);
+		}
+		else{
+			*puiValue = *puiValue | (pcStr[ucCharCounter] - '0');
+		}
 	}
+	return OK;
+}
+
+void ReplaceCharactersInString(char pcString[], char cOldChar, char cNewChar){
+  
+  unsigned char ucCharNumber;
+  
+  for(ucCharNumber = 0; pcString[ucCharNumber] != '\0'; ucCharNumber++){
+    if(pcString[ucCharNumber] == cOldChar){
+      pcString[ucCharNumber] = cNewChar;
+    }
+  }
 }
 
 void DecodeTokens(void){
+  
+  unsigned char ucTokenCounter;
+  
+  for(ucTokenCounter = 0; ucTokenCounter < ucTokenNr; ucTokenCounter++){
 
-	unsigned char ucTokenCounter;
-
-	Token *tValue;
-
-	for (ucTokenCounter = 0; ucTokenCounter < MAX_TOKEN_NR; ++ucTokenCounter)
-	{
-		tValue = &asToken[ucTokenCounter];
-		if(eStringToKeyword(tValue->uValue.pcString, &tValue->uValue.eKeyword) == OK){
-			tValue->eType == KEYWORD;
-		}
-		else if(eHexStringToUInt(tValue->uValue.pcString, &tValue->uValue.uiNumber) == OK){
-			tValue->eType == NUMBER; 
-		}
-		else{
-			tValue->eType == STRING;
-		}
-	}
+    if(eStringToKeyword(asToken[ucTokenCounter].uValue.pcString, &asToken[ucTokenCounter].uValue.eKeyword) == OK){
+      asToken[ucTokenCounter].eType = KEYWORD;
+    }
+    else if(eHexStringToUInt(asToken[ucTokenCounter].uValue.pcString, &asToken[ucTokenCounter].uValue.uiNumber) == OK){
+      asToken[ucTokenCounter].eType = NUMBER;
+    }
+    else{
+      asToken[ucTokenCounter].eType = STRING;
+    }
+  }
 }
 
-//troll xDDDDDDDDD
+
+void DecodeMsg(char *pcString){
+  ucTokenNr = ucFindTokensInString(pcString);
+  ReplaceCharactersInString(pcString, ' ', '\0');
+  DecodeTokens();
+}
+
+//***********************MAIN*********************
+
+char napis[]="0x2137 siemanko store";
+
+int main(void){
+  DecodeMsg(napis);
+}
